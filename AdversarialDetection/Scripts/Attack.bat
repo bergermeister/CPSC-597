dc2680148333ad96774a09b490ae1e13c0296609
Eisenberger, Edward <edward.eisenberger@live.com>
2021-02-11 14:41:18 -0500

    Updated attack script to take epsilon as an argument

--- AdversarialDetection/Scripts/Attack.bat
+++ AdversarialDetection/Scripts/Attack.bat
@@ -2,6 +2,6 @@ python AdversarialDetection.py --dataset=mnist                                 ^
                                --data_path=E:\Projects\Dataset\mnist\processed ^
                                --batch_size=1                                  ^
                                --mode=adversary                                ^
-                               --epsilon=0.1                                   ^
+                               --epsilon=%1                                    ^
                                --cuda=True                                     ^
                                --cnn=E:\Projects\CPSC-597\AdversarialDetection\State\cnn.model
